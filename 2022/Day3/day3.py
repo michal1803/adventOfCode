83933a16e3ca0a409beb5536ee850826c32e517b
michal1803 <michaw49@gmail.com>
2022-12-12 22:47:51 +0100

    Day 3 part 1 done

--- 2022/Day3/day3.py
+++ 2022/Day3/day3.py
@@ -1,10 +1,31 @@
 # day 3
+import numpy as np
+pSum = 0
+counter = 1
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 file = open("2022/Day3/input.txt", "r")
 
 for i in file:
-    lenOfString = len(i)
-    middleOfString = lenOfString/2
-    fPart =i[0:middleOfString]
-    sPart =i[middleOfString:lenOfString]
-    print(fPart + "   " + sPart)
+    hash = i.strip()
+    lenOfString = len(hash)
+    middleOfString = lenOfString
+    fPart = i[:middleOfString//2]
+    sPart = i[middleOfString//2:lenOfString]
+    fPartList = list(fPart)
+    sPartList = list(sPart)
+    char = np.intersect1d(fPartList, sPartList)
+    charStr = np.array2string(char)
+
+    if charStr.isupper():
+        newChar = charStr.lower()
+        priority = alphabet.index(newChar[2])
+        priority += 27
+    else:
+        priority = alphabet.index(charStr[2])
+        priority +=1
+
+    pSum += priority
+    print(counter,charStr[2],pSum)
+    counter += 1
