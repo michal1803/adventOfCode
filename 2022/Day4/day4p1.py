6f9dde8c1d2cbf16b29718125e654e8aca355fff
michal1803 <michaw49@gmail.com>
2022-12-14 19:49:32 +0100

    Day 4 part 1 complete

---
+++ 2022/Day4/day4p1.py
@@ -0,0 +1,23 @@
+# day 4
+
+file = open("2022/Day4/input.txt", "r")
+
+counter = 0
+
+for i in file:
+    pairs = i.split(",")
+    fPair = pairs[0].strip().split("-")
+    sPair = pairs[1].strip().split("-")
+    ftoListOfNum = list(map(int, fPair))
+    stoListOfNum = list(map(int, sPair))
+
+    if ftoListOfNum[0] > stoListOfNum[0]:
+        if ftoListOfNum[1] <= stoListOfNum[1]:
+            counter += 1
+    elif ftoListOfNum[0] == stoListOfNum[0]:
+        counter += 1
+    else:
+        if ftoListOfNum[1] >= stoListOfNum[1]:
+            counter += 1
+        
+    print(ftoListOfNum, stoListOfNum, counter)
